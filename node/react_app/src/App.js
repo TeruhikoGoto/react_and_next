ee0486fbdfc7b79bc78b5ff1ac8e9f41d507f9d9
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2022-12-19 22:36:26 +0900

    setstate

--- node/react_app/src/App.js
+++ node/react_app/src/App.js
@@ -1,22 +1,30 @@
 import React, {Component} from 'react'
 import './App.css';
-import Rect from './Rect'
 
 class App extends Component {
   constructor(props){
-    super()
-    this.title = props.title
-    this.message = props.message
+    super(props);
+    this.state = {
+      msg: 'Hello',
+      count: 0,
+    }
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+        msg: "[count: " + this.state.count + "]"
+      })
+    }, 1000)
   }
   render(){
     return(
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <p className="subtitle">draw rectangle.</p>
-          <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-          <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-          <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+          <p className="subtitle">Count number.</p>
+          {/* state.msgの値は更新される。値が更新されると、自動的に表示が更新される(ReactDOM.render) */}
+          <p className="alert alert-warning">{this.state.msg}</p>
+          {/* props.msgの値は更新されない。 */}
+          <p className="alert alert-dark">{this.props.msg}</p>
         </div>
       </div>
     )
