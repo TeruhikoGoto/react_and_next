a148867b198f4f6eb59a85ddaf89a024220a95c0
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2022-12-20 16:40:52 +0900

    [update]ステートによって表示を切り替え

--- node/react_app/src/App.js
+++ node/react_app/src/App.js
@@ -1,22 +1,45 @@
 import React, {Component} from 'react'
 import './App.css';
-import Rect from './Rect'
 
 class App extends Component {
   constructor(props){
-    super()
-    this.title = props.title
-    this.message = props.message
+    super(props)
+    this.state = {
+      counter: 0,
+      msg: 'count start!',
+      flg: true,
+    }
+    this.doAction = this.doAction.bind(this)
   }
+
+  doAction(e){
+    this.setState({
+      counter: this.state.counter + 1,
+      msg: this.state.counter,
+      flg: !this.state.flg,
+    })
+  }
+
   render(){
     return(
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <p className="subtitle">draw rectangle.</p>
-          <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-          <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-          <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+          <p className="subtitle">Count number.</p>
+          {this.state.flg ?
+            <div className="alert alert-primary text-right">
+              <p className="h5">count: {this.state.msg}</p>
+            </div>
+          :
+            <div className="alert alert-warning text-left">
+              <p className="h5">{this.state.msg}です。</p>
+            </div>
+          }
+          <div className="text-center">
+            <button className="btn btn-primary"
+              onClick={this.doAction}>
+              Click</button>
+          </div>
         </div>
       </div>
     )
