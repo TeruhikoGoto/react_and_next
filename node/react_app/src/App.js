92f66f28f46d336f5edc7a93afbf351aeeb83405
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2022-12-22 16:49:43 +0900

    [update]コンテキストを使う

--- node/react_app/src/App.js
+++ node/react_app/src/App.js
@@ -1,23 +1,44 @@
 import React, {Component} from 'react'
-import './App.css';
-import Rect from './Rect'
+import './App.css'
+
+let data = {title: 'React-Context',
+  message: 'this is sample message.' }
+
+const SampleContext = React.createContext(data)
 
 class App extends Component {
-  constructor(props){
-    super()
-    this.title = props.title
-    this.message = props.message
-  }
   render(){
     return(
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <p className="subtitle">draw rectangle.</p>
-          <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-          <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-          <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+        <Title></Title>
+        <Message></Message>
         </div>
+      </div>
+    )
+  }
+}
+
+class Title extends Component {
+  static contextType = SampleContext
+  
+  render(){
+    return(
+      <div className="card p-2 my-3">
+        <h2>{this.context.title}</h2>
+      </div>
+    )
+  }
+}
+
+class Message extends Component {
+  static contextType = SampleContext
+  
+  render(){
+    return(
+      <div className="alert alert-primary">
+        <p>{this.context.message}</p>
       </div>
     )
   }
