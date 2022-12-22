35b4203b718d80f9830a8b41baa9e7ec084725e3
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2022-12-22 15:41:53 +0900

    [update]フォームを使う

--- node/react_app/src/App.js
+++ node/react_app/src/App.js
@@ -6,51 +6,51 @@ class App extends Component {
 
   constructor(props){
     super(props)
+    console.log(this)
+    this.state = {
+      title: 'input form',
+      message: 'type your name',
+    }
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
+  }
+
+  // フォームの入力があった時に実行される
+  doChange(event) {
+    console.log(this)
+    this.input = event.target.value;
+  }
+
+  // ボタンがクリックされた時に実行される。
+  doSubmit(event) {
+    console.log(this)
+    this.setState({
+      title: 'send form',
+      message: 'Hello, ' + this.input + '!!'
+    })
+    event.preventDefault()
   }
 
   render(){
+    console.log(this)
     return(
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <Message title="Children!">
-            これはコンポーネント内のコンテンツです。
-            まるでテキストを分割し、リストにして表示します。
-            改行は必要ありません。
-          </Message>
+          <h4>{this.state.title}</h4>
+          <p className="card h5 p-3">{this.state.message}</p>
+          <div className="alert alert-primary mt-3">
+            <form onSubmit={this.doSubmit}>
+              <div className="form-group">
+                <label>Message:</label>
+                <input type="text" className="form-control"
+                  onChange={this.doChange} />
+              </div>
+              <input type="submit" className="btn btn-primary"
+                value="Click" />
+            </form>
+          </div>
         </div>
-      </div>
-    )
-  }
-}
-
-class Message extends Component {
-  li = {
-    fontSize: "14pt",
-    fontWeight: "bold",
-    color: "#090",
-  }
-
-  render(){
-    let content = this.props.children
-    let arr = content.split('。')
-    let arr2 = [];
-    // trim(): 文字列の両端の空白を除去する。
-    // 行がblankじゃなければ、arr2にpushする。
-    for(let i = 0;i < arr.length;i++){
-      if (arr[i].trim() !== ''){
-        arr2.push(arr[i]);
-      }
-    }
-    // list: liの集合。
-    let list = arr2.map( (value, key)=>(
-      <li className="list-group-item" style={this.li}
-        key={key}>{key + 1}. {value} .</li>)
-    )
-    return(
-      <div>
-        <h2>{this.props.title}</h2>
-        <ol className="list-group">{list}</ol>
       </div>
     )
   }
