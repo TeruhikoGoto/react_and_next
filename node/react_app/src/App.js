5a74fe492841ca9353a3f8ee822b0ef8ebf72493
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2023-01-04 13:44:46 +0900

    複雑な独自フック

--- node/react_app/src/App.js
+++ node/react_app/src/App.js
@@ -2,23 +2,41 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 
 // 数字を保管するステートと、数字に1カウントする関数を備える独自フック。
-function useCounter() {
-  const [num, setNum] = useState(0)
+const useTax = (t1, t2)=> {
+  const [price, setPrice] = useState(1000)
+  const [tx1] = useState(t1)
+  const [tx2] = useState(t2)
 
-  const count = ()=>{
-    setNum(num + 1)
+  const tax = ()=> {
+    return Math.floor(price * (1.0 + tx1 / 100))
   }
 
-  return [num, count]
+  const reduced = ()=> {
+    return Math.floor(price * (1.0 + tx2 / 100))
+  }
+
+  return [price, tax, reduced, setPrice]
 }
 
 function AlertMessage(props) {
   // 独自フックを呼び出すときはこの形。
-  const [counter, plus] = useCounter()
+  const [price, tax, reduced, setPrice] = useTax(10, 8)
+
+  // ここでsetPrice。setPriceを呼び出すと、useTaxの独自フックが呼び出され、price, tax, reducedが更新される。
+  const DoChange = (e)=> {
+    let p = e.target.value
+    setPrice(p)
+  }
+
   return(
-    <div className="alert alert-primary h5 text-center">
-      <h4>count: {counter} .</h4>
-      <button onClick={plus} className="btn btn-primary">count</button>
+    <div className="alert alert-primary h5">
+      <p className="h5">通常税率: {tax()} 円.</p>
+      <p className="h5">軽減税率: {reduced()} 円.</p>
+      <div className="form-group">
+        <label className="form-group-label">Price:</label>
+        <input type="number" className="form-control"
+          onChange={DoChange} value={price} />
+      </div>
     </div>
   )
 }
