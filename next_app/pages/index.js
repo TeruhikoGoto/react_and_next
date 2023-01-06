52aa23a247eafc1c3b0e3e3e30c4e6b73c864369
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2023-01-06 15:09:50 +0900

    index.jsを作成する

---
+++ next_app/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  let title = "Next.js page"
+  let message = "React Next.js sample page."
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous" />
+      </Head>
+
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <h4 className="my-3">{title}</h4>
+        <div className="alert alert-primary text-center">
+          <p className="h5">{message}.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
