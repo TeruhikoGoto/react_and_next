ff21a797e39cebfa65bbc0396d8f60823d5dd71d
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2023-01-24 21:25:24 +0900

    fetch

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,14 +1,33 @@
+import {useState} from 'react'
 import Layout from '../components/layout'
-import MyImage from '../components/image'
-import Link from 'next/link'
 
 export default function Home() {
+  const url = './data.json'
+  const [data, setData] = useState({message:'', data:[]})
+
+  // 非同期で実行
+  fetch(url) // responceオブジェクトというのが返却される
+    .then(res=> res.json()) // res = responceオブジェクト。res.json()して、返却する。
+    .then(res=> setData(res)) // res = res.json()。setDataで、ステートに保管。messageとdataで、それぞれ対応してるところに保管されるよう？
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">Welcome to next.js!</h5>
-          <MyImage fname="image.png" size="300"></MyImage>
+          <h5 className="mb-4">{data.message}</h5>
+          <table className="table bg-white">
+            <thead className="table-dark">
+              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+            </thead>
+            <tbody>
+              {data.data.map((value, key) => (
+                <tr key={key}>
+                  <th>{value.name}</th>
+                  <th>{value.mail}</th>
+                  <th>{value.age}</th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </Layout>
     </div>
