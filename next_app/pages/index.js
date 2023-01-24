1e5d76e0c94b0fc1ec0460edd98bed419354f9ed
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2023-01-24 20:02:24 +0900

    [update]Imageコンポーネント実装

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../components/layout'
+import MyImage from '../components/image'
 import Link from 'next/link'
 
 export default function Home() {
@@ -7,13 +8,7 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">Welcome to next.js!</h5>
-          <div className="card p-3 text-center">
-            <Link href="/other">
-              <button className="btn btn-primary">
-                go to Other &gt;&gt;
-              </button>
-            </Link>
-          </div>
+          <MyImage fname="image.png" size="300"></MyImage>
         </div>
       </Layout>
     </div>
