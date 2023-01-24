9d5a20ae9707267a376ad86e71c986a92634c0fd
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2023-01-24 22:01:05 +0900

    swr実装

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,31 +1,29 @@
-import {useState} from 'react'
 import Layout from '../components/layout'
+import useSWR from 'swr'
 
 export default function Home() {
-  const url = './data.json'
-  const [data, setData] = useState({message:'', data:[]})
+  const fetcher = url => fetch(url).then(r => r.json())
+  const { data } = useSWR('/data.json', fetcher)
 
-  // 非同期で実行
-  fetch(url) // responceオブジェクトというのが返却される
-    .then(res=> res.json()) // res = responceオブジェクト。res.json()して、返却する。
-    .then(res=> setData(res)) // res = res.json()。setDataで、ステートに保管。messageとdataで、それぞれ対応してるところに保管されるよう？
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">{data.message}</h5>
-          <table className="table bg-white">
-            <thead className="table-dark">
+          <h5 className="mb-4">
+            { data != undefined ? data.message : 'error...' }
+          </h5>
+          <table className="table table-dark">
+            <thead className="">
               <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
             </thead>
             <tbody>
-              {data.data.map((value, key) => (
+              {data != undefined ? data.data.map((value, key) => (
                 <tr key={key}>
                   <th>{value.name}</th>
                   <th>{value.mail}</th>
                   <th>{value.age}</th>
                 </tr>
-              ))}
+              )) : <tr><th></th><td>err</td><td></td></tr>}
             </tbody>
           </table>
         </div>
