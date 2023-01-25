2c115dfc5fc45b26bd8e02e1c7a61d5c2a7c6f20
TeruhikoGoto <82075734+TeruhikoGoto@users.noreply.github.com>
2023-01-25 16:12:45 +0900

    テキストデータを取得

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -2,30 +2,18 @@ import Layout from '../components/layout'
 import useSWR from 'swr'
 
 export default function Home() {
-  const fetcher = url => fetch(url).then(r => r.json())
-  const { data } = useSWR('/data.json', fetcher)
+  // テキストの形で返すfetchの即時関数
+  // なんで...argsが'/data.txt'だと判別できてる？
+  const func = (...args) => fetch(...args).then(res => res.text())
+  const { data, err } = useSWR('/data.txt', func)
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">
-            { data != undefined ? data.message : 'error...' }
+            { data }
           </h5>
-          <table className="table table-dark">
-            <thead className="">
-              <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-            </thead>
-            <tbody>
-              {data != undefined ? data.data.map((value, key) => (
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <th>{value.mail}</th>
-                  <th>{value.age}</th>
-                </tr>
-              )) : <tr><th></th><td>err</td><td></td></tr>}
-            </tbody>
-          </table>
         </div>
       </Layout>
     </div>
